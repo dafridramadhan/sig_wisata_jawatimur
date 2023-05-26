7c32620460abce6ac05efaa902153ba69b8c2435
unknown <dafridramadhan@gmail.com>
2023-05-26 08:24:54 +0700

    Init Project

---
+++ admin/menu_sidebar.php
@@ -0,0 +1,26 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-secondary sidebar sidebar-dark accordion" id="accordionSidebar">
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link" href="index.php">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="tampil_data.php">
+            <i class="fas fa-fw fa-user-friends"></i>
+            <span>Data Wisata</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="tambah_data.php">
+            <i class="fas fa-fw fa-user-friends"></i>
+            <span>Tambah Data</span></a>
+    </li>
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+</ul>
+<!-- End of Sidebar -->
